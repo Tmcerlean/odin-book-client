43c4c877eb21119c93b570aac3a6094f19b43d46
Thomas McErlean <tjmcerlean@googlemail.com>
2021-07-16 12:26:44 +0100

    add feed outline and test stories array

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,10 @@
+import Stories from './Stories';
+
 const Feed: React.FC = () => {
     return (
         <div className="">
             {/* Stories */}
+            <Stories />
             {/* Input Box */}
             {/* Posts */}
         </div>    
