52c173bf6b0fa41cd2e7678be4d0f743f640156e
Thomas McErlean <tjmcerlean@googlemail.com>
2021-08-05 12:18:28 +0100

    add useRef focus for add comment button

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import formatDistance from "date-fns/formatDistance";
 import { PostObject } from '../@types/types';
 import { ChatIcon, ThumbUpIcon } from "@heroicons/react/outline";
@@ -12,6 +12,10 @@ const Post: React.FC<PostProps> = ({ content }) => {
 
 
     const [commentContent, setCommentContent] = useState("");
+
+    const commentInput = useRef<HTMLInputElement>(null);
+
+    const handleFocus = () => commentInput?.current?.focus();
 
     const submitPost = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -40,15 +44,23 @@ const Post: React.FC<PostProps> = ({ content }) => {
                     <ThumbUpIcon className="h-5 text-gray-600 mr-1" />
                     <p className="text-xs sm:text-sm xl:text-base text-gray-600">Like</p>
                 </div>
-                <div className="flex flex-1 justify-center m-1 p-2 items-center hover:bg-gray-100 cursor-pointer rounded-sm">
+                <div 
+                    className="flex flex-1 justify-center m-1 p-2 items-center hover:bg-gray-100 cursor-pointer rounded-sm"
+                    onClick={handleFocus}
+                    onKeyDown={(event) => {
+                        if (event.key === 'Enter') {
+                            handleFocus();
+                        }
+                    }}
+                >
                     <ChatIcon className="h-5 text-gray-600 mr-1" />
                     <p className="text-xs sm:text-sm xl:text-base text-gray-600">Comment</p>
                 </div>
             </div>
             <div className="">
-                <div className="flex">
+                <div className="flex relative mt-2">
                     <img 
-                        className="rounded-full"
+                        className="rounded-full mr-1"
                         src="https://i.ibb.co/42drhRP/mcerleantom-79390488-2535626826491703-1307724872187531610-n-1.jpg"
                         alt=""
                         width="40"
@@ -57,12 +69,14 @@ const Post: React.FC<PostProps> = ({ content }) => {
                     <form className="flex flex-1" onSubmit={submitPost}>
                         <input 
                             type="text" 
-                            placeholder="What's your story?"
-                            className="rounded-full h-12 bg-gray-100 flex-grow px-5 border-none focus:outline-none"
+                            placeholder="Add a comment..."
+                            className="rounded-full bg-gray-100 flex-grow px-5 border-none focus:outline-none"
                             value={commentContent}
                             onChange={(e) => setCommentContent(e.target.value)}
+                            ref={commentInput}
                         />
                     </form>
+                    <div className="absolute bottom-0 left-7 bg-green-400 border-2 border-white h-3 w-3 rounded-full"></div>
                 </div>
             </div>
         </div>
